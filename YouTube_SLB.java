596f4509fa9ac77959405cae354c4c993b70d2e9
wsong-nj <31369093+wsong-nj@users.noreply.github.com>
2018-08-20 22:14:41 +0800

    Delete YouTube_SLB.java

--- YouTube_SLB.java
+++
@@ -1,7 +0,0 @@
-class jth extends BroadcastReceiver{
-  public void onReceive(Context paramContext, Intent paramIntent){
-    localIntent.setComponent(new ComponentName(localContext,
-      "com.google.android.gms.analytics.AnalyticsService"));
-    localContext.startService(localIntent);
-  }
-}
