69536af35fbb0d7c10d944d46f48036c133fa3ff
wsong-nj <31369093+wsong-nj@users.noreply.github.com>
2018-08-20 22:14:26 +0800

    Delete WhatsAppMessenger_SLB.java

--- WhatsAppMessenger_SLB.java
+++
@@ -1,6 +0,0 @@
-public static class ExternalMediaStateReceiver extends BroadcastReceiver{
-  public void onReceive(Context paramContext, Intent paramIntent){
-    paramContext.startService(paramIntent.setClass(paramContext,
-      ExternalMediaManager.class));
-  }
-}
