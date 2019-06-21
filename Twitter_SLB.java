16a3a7e1300ea9c45631680399b82c9fe0c9ee57
wsong-nj <31369093+wsong-nj@users.noreply.github.com>
2019-06-21 11:17:35 +0800

    Delete Twitter_SLB.java

--- Twitter_SLB.java
+++
@@ -1,7 +0,0 @@
-public class OemIntentReceiver extends BroadcastReceiver{
-  public static void a(Context paramContext){
-    localIntent.setClassName("com.twitter.twitteroemhelper",
-     "com.twitter.twitteroemhelper.OemHelperService");
-    paramContext.startService(localIntent);
-  }
-}
